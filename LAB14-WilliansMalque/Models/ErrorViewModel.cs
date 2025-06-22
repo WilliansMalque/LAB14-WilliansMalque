2f7ce3c3e06d3338fa3d96327a00e01d00b8ae66
Willians Leonardo Malque Zurita <143668007+WilliansMalque@users.noreply.github.com>
2025-06-22 15:48:34 -0500

    Primer commit del proyecto .NET

---
+++ LAB14-WilliansMalque/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LAB14_WilliansMalque.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
